24f98631f719214655f951adcfb08c826ae9f50e
skills-outside-school1 <skillsoutsideschool@gmail.com>
2025-09-16 21:08:38 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,8 +13,14 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"], // Add this to handle SVGs as React components
+      use: ["@svgr/webpack"], // Add this to handle SVGs as React component
     });
+     // Temporarily disable css minimization to get better error messages
+    if (config.optimization && config.optimization.minimizer) {
+      config.optimization.minimizer = config.optimization.minimizer.filter(
+        (minimizer) => minimizer.constructor.name !== 'CssMinimizerPlugin'
+      );
+    }
 
     return config;
   },
