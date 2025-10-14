907454dcdf6bd66139c37ebcafc4acdf57ba0e9b
Oluwasegun Benjamin Tobi <benjaminsegun38@gmail.com>
2025-10-14 10:53:16 +0100

    comment out the interactive map from the homepage

--- src/components/home/Home-Atoms/Hero.js
+++ src/components/home/Home-Atoms/Hero.js
@@ -1,18 +1,14 @@
-// import ReuseHero from "@/components/shared/Atoms/Hero-Atoms/ReusableMaps/Hero";
+import ReuseHero from "@/components/shared/Atoms/Hero-Atoms/ReusableMaps/Hero";
 import React from "react";
 
 export default function Hero() {
   return (
-    <div>
-      {/* commented out the interactive map */}
-
-      {/* <div className="w-full h-[fixed]">
-        <ReuseHero
-          overlayOpacity="bg-opacity-5"
-          //  backgroundImage="assets/maps/MapChart_Map.png"
-          backgroundImage="/assets/images/home/1010.png"
-        />
-      </div> */}
+    <div className="w-full h-[fixed]">
+      <ReuseHero
+        overlayOpacity="bg-opacity-5"
+        //  backgroundImage="assets/maps/MapChart_Map.png"
+        backgroundImage="/assets/images/home/1010.png"
+      />
     </div>
   );
 }
