9179ce31f8be5b9bb2347caf5e5086dc3a69f9c9
Bensaxxy <benjaminsegun38@gmail.com>
2025-10-24 08:56:10 +0100

    marging files

--- src/components/Our-Work/Data/Data-Atoms/DataSub.js
+++ src/components/Our-Work/Data/Data-Atoms/DataSub.js
@@ -5,7 +5,7 @@ import React from "react";
 const DataSub = () => {
   return (
     <div className="w-full h-auto px-2 md:px-14 lg:px-[110px] flex">
-      <div className=" bg-governblue rounded-lg py-3 px-4 flex flex-col gap-3">
+      <div className=" bg-governblue rounded-xl py-3 px-4 flex flex-col gap-3">
         <Image
           src="/assets/icons/data.svg"
           alt="data-icons"
@@ -15,7 +15,7 @@ const DataSub = () => {
 
         <H2>Data</H2>
         <P>Evidence that drives Impact</P>
-        <P>Data isn't just a tool. It's our compass.</P>
+        <p>Data isn't just a tool. It's our compass.</p>
       </div>
       <div className=" ">
         <div></div>
