27a5d04b789a891d8d8b063337d10d030d07ac96
Daniel Asakpa <danielasakpa@gmail.com>
2025-11-12 09:29:15 +0100

    fix: update import paths for Admin components in AdminLayout

--- src/components/layouts/AdminLayout.jsx
+++ src/components/layouts/AdminLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import AdminNavbar from "@/components/admin/AdminNavbar";
-import AdminSidebar from "@/components/admin/AdminSidebar";
-import AdminFooter from "@/components/admin/AdminFooter";
+import AdminNavbar from "@/components/Admin/Admin-Atoms/AdminNavbar";
+import AdminSidebar from "@/components/Admin/Admin-Atoms/AdminSidebar";
+import AdminFooter from "@/components/Admin/Admin-Atoms/AdminFooter";
 
 export default function AdminLayout({ children }) {
   return (
