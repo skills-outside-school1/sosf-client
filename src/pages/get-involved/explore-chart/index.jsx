b001870d117fe0e096fe876b2f2d649f6db27d7e
TongstonIt <it@tongston.com>
2025-12-11 07:20:39 +0800

    made and commented out the explore chart

--- src/pages/get-involved/explore-chart/index.jsx
+++ src/pages/get-involved/explore-chart/index.jsx
@@ -1,22 +1,23 @@
-import {useEffect} from "react"
-import StructureChartContainer from "@/components/structure-chart/Main/Main-Container/StructureChartConteiner"
-import Head from "next/head"
+import { useEffect } from "react";
+
+import Head from "next/head";
 
 const Index = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-    return (
-        <>
-            <Head>
-                <title> Explore Structure Chart | Skills Outside School Foundation </title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>
+          {" "}
+          Explore Structure Chart | Skills Outside School Foundation{" "}
+        </title>
+      </Head>
 
-            <div className="w-full h-[fixed] justify-start items-start">
-                <StructureChartContainer />
-            </div>
-        </>
-    )
-}
+      <div className="w-full h-[fixed] justify-start items-start"></div>
+    </>
+  );
+};
 
-export default Index
+export default Index;
