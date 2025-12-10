c68dc62f4a1dc039da949f6209189312309cdfc0
TongstonIt <it@tongston.com>
2025-12-11 02:08:20 +0800

    accept current changes from muhammad

--- src/components/structure-chart/Main/Main-Container/StructureChartConteiner.js
+++ src/components/structure-chart/Main/Main-Container/StructureChartConteiner.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import HeroChart from "../Main-Atoms/hero-chart";
 import PartnerSection from "../Main-Atoms/partner-section";
 import InvestmentSection from "../Main-Atoms/investment-section";
@@ -32,39 +31,4 @@ export default function StructureChartContainer() {
       <Subscribe />
     </div>
   );
-=======
-import HeroChart from "../Main-Atoms/hero-chart"
-import PartnerSection from "../Main-Atoms/partner-section"
-import InvestmentSection from "../Main-Atoms/investment-section"
-import GovernanceSection from "../Main-Atoms/governance-section"
-import ParticipationSection from "../Main-Atoms/participation-section"
-import { Element } from "react-scroll"
-import WorkNav from "@/components/About/Who-We-Are/Who-We-Are-Atoms/MoreAboutUs"
-import MainSub from "@/components/Get-Involved/Main/Main-Atoms/MainSub"
-import Pathway from "@/components/Get-Involved/Main/Main-Atoms/Pathway"
-
-export default function StructureChartContainer() {
-  return (
-      <div className="w-full h-[fixed]">
-   
-        <Element name="hero-section">
-            <HeroChart />
-        </Element>
-        <Element name="partner-section">
-            <PartnerSection />
-        </Element>
-        <Element name="invest-section">
-            <InvestmentSection />
-        </Element>
-        <Element name="govern-section">
-            <GovernanceSection />
-        </Element>
-        <Element name="participate-section">
-            <ParticipationSection />
-          </Element>
-          <Pathway />
-          <WorkNav/>
-        </div>
-  )
->>>>>>> 1f45b5d5737c81ed39d9106182f47de4a6036230
 }
