85630d2e5bf4ef4e59f8fb1e396f7aff24639968
daniel-itics <itics@tongston.com>
2025-12-19 10:31:56 +0100

    feat: add initial structure for Grants

---
+++ src/pages/interventions/grants/large-grants/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const Index = () => {
+  return (
+    <div className="flex flex-col w-full h-auto overflow-hidden main">
+      <Head>
+        {/* in page seo for the history page  */}
+        <title>Grants | Skills Outside School Foundation</title>
+        <meta name="description" content="" />
+      </Head>
+    </div>
+  );
+};
+
+export default Index;
