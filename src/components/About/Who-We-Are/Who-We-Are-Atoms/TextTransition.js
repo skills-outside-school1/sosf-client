a2b86b60374727a346d7e1b39f599bc03b278b26
TongstonIt <it@tongston.com>
2026-04-27 14:55:12 +0100

    made some canges to resolve deployment errors

--- src/components/About/Who-We-Are/Who-We-Are-Atoms/TextTransition.js
+++ src/components/About/Who-We-Are/Who-We-Are-Atoms/TextTransition.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Transition from "@/components/shared/Atoms/Text-Transitions/Transition";
 
-export default function TextTransition() {
-  const phrases = ["Hello, World!", "Welcome to React", "Framer Motion is awesome"];
+const TextTransition = () => {
+  const phrases = [
+    "Hello, World!",
+    "Welcome to React",
+    "Framer Motion is awesome",
+  ];
 
   return (
-    <div  className="">
+    <div className="">
       {/* Pass the array of phrases to the Transition component */}
-      <Transition texts={phrases} interval={3000} className="text-4xl font-bold text-black " />
+      <Transition
+        texts={phrases}
+        interval={3000}
+        className="text-4xl font-bold text-black "
+      />
     </div>
   );
-}
+};
+export default TextTransition;
